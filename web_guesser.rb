344052b0f2b690e18ab1cefd9094e87a2380a220
Tyler Graham <Tyler@Tylers-MacBook-Pro.local>
2015-03-25 12:38:50 -0600

    Adding guess response message functionality

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,11 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require_relative 'checker'
 
 number = rand(100)
 
 get '/' do
-  erb :index, :locals => {:number => number}
+  guess = params['guess'].to_i
+  message = Checker.check_guess(number, guess)
+  erb :index, :locals => {:number => number, :guess => guess, :message => message}
 end
